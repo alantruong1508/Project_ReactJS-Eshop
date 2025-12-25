e15ab36cb6814754170667583d2890f57b93aba3
An Truong <truongan0988364318@gmail.com>
2025-12-25 15:29:41 -0600

    fix-error-unse-function

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,4 @@
-export default function Button({
-  text,
-  bgColor,
-  textColor,
-  handler = () => {},
-}) {
+export default function Button({ text, bgColor, textColor }) {
   return (
     <button
       className={`${bgColor} ${textColor} cursor-pointer hover:scale-105 duration-300 py-2 px-8 rounded-full relative z-10`}
